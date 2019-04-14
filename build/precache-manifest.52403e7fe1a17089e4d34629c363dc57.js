797351da89cddec7033475109909530c706f064a
Amit Pandit <35424309+amitpandit09@users.noreply.github.com>
2019-04-13 20:21:38 -0400

    first time push from local system

---
+++ build/precache-manifest.52403e7fe1a17089e4d34629c363dc57.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "c42370b02a24df071939",
+    "url": "/static/js/main.cc960f81.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "c42370b02a24df071939",
+    "url": "/static/css/main.2201412d.chunk.css"
+  },
+  {
+    "revision": "a54908fecd6797566a714a72b6983aa5",
+    "url": "/index.html"
+  }
+];
